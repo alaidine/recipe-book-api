a1ed69e84f21aac52f357e7249b7c9585d00a9c4
alaidine <aa.alimlinde@gmail.com>
2024-01-01 20:22:44 +0400

    connection to the database tested

--- src/main.rs
+++ src/main.rs
@@ -36,16 +36,41 @@ pub struct AppState {
     db: Arc<Mutex<Client>>,
 }
 
+pub async fn get_recipes(
+    State(state): State<Arc<AppState>>
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let res = match state
+        .db
+        .lock()
+        .await
+        .execute("SELECT * FROM recipes")
+        .await
+    {
+        Ok(res) => res,
+        Err(err) => return Err((StatusCode::INTERNAL_SERVER_ERROR, err.to_string())),
+    };
+
+    Ok((StatusCode::OK, Json(res)))
+}
+
 #[shuttle_runtime::main]
 async fn main(
     #[shuttle_turso::Turso(
-        addr="{secrets.DATABASE_URL}")] client: Client,
+        addr="{secrets.TURSO_ADDR}",
+        token="{secrets.TURSO_TOKEN}"
+    )] client: Client,
     #[shuttle_secrets::Secrets] secret_store: SecretStore
 ) -> ShuttleAxum {
+    client.batch([
+        "CREATE TABLE IF NOT EXISTS recipes (name text, content text)"
+    ])
+    .await
+    .unwrap();
     let db = Arc::new(Mutex::new(client));
     let state = Arc::new(AppState { db: db.clone() });
     let router = Router::new()
         .route("/", get(homepage))
+        .route("/recipes", get(get_recipes))
         .with_state(state);
 
     Ok(router.into())

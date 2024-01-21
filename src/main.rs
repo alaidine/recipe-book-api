5deff181b02c0e40c5aa478c8bb1a9d3a0e6bbcf
alaidine <aa.alimlinde@gmail.com>
2024-01-21 17:39:21 +0400

    add create recipe

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 
+use std::fmt;
 use axum::{
     extract::State,
     http::StatusCode,
@@ -8,6 +9,7 @@ use axum::{
     Router,
     Json,
 };
+use serde::{Deserialize, Serialize};
 use shuttle_secrets::SecretStore;
 use shuttle_axum::ShuttleAxum;
 use libsql_client::client::Client;
@@ -16,9 +18,14 @@ use anyhow::anyhow;
 use tokio::sync::Mutex;
 use std::sync::Arc;
 
+#[derive(Serialize, Deserialize)]
 struct Recipe {
     name: String,
     content: String,
+}
+
+pub struct AppState {
+    db: Arc<Mutex<Client>>,
 }
 
 async fn homepage() -> impl IntoResponse {
@@ -32,8 +39,42 @@ Here are the following routes:
 "#
 }
 
-pub struct AppState {
-    db: Arc<Mutex<Client>>,
+pub async fn create_recipe(
+    State(state): State<Arc<AppState>>,
+    Json(recipe): Json<Recipe>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let res = match state
+        .db
+        .lock()
+        .await
+        .execute(
+            format!("INSERT INTO recipes (name, content) VALUES ({}, {})", recipe.name, recipe.content).as_str()
+        )
+        .await
+    {
+        Ok(res) => res,
+        Err(err) => return Err((StatusCode::INTERNAL_SERVER_ERROR, err.to_string())),
+    };
+
+    Ok((StatusCode::OK, Json(res)))
+}
+
+pub async fn delete_recipes(
+    State(state): State<Arc<AppState>>,
+    Json(recipe): Json<Recipe>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let res = match state
+        .db
+        .lock()
+        .await
+        .execute(format!("DELETE FROM recipes WHERE name='{}'", recipe.name).as_str())
+        .await
+    {
+        Ok(res) => res,
+        Err(err) => return Err((StatusCode::INTERNAL_SERVER_ERROR, err.to_string())),
+    };
+
+    Ok((StatusCode::OK, Json(res)))
 }
 
 pub async fn get_recipes(
@@ -71,6 +112,8 @@ async fn main(
     let router = Router::new()
         .route("/", get(homepage))
         .route("/recipes", get(get_recipes))
+        .route("/create", get(create_recipe))
+        .route("/delete", get(delete_recipes))
         .with_state(state);
 
     Ok(router.into())
